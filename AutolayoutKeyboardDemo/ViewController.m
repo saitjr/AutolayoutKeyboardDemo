2b83d386231218113847996f7dc7ce7c6e9ee8e0
TangJR <tangjr.work@gmail.com>
2015-04-29 11:54:06 +0800

    update

--- AutolayoutKeyboardDemo/ViewController.m
+++ AutolayoutKeyboardDemo/ViewController.m
@@ -10,18 +10,66 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *bottomConstraint;
+
 @end
 
 @implementation ViewController
 
+- (void)dealloc {
+    
+    // 移除键盘通知
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // 注册键盘通知
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillChangeFrameNotification:) name:UIKeyboardWillChangeFrameNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHideNotification:) name:UIKeyboardWillHideNotification object:nil];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)keyboardWillChangeFrameNotification:(NSNotification *)notification {
+    
+    // 获取键盘基本信息（动画时长与键盘高度）
+    NSDictionary *userInfo = [notification userInfo];
+    CGRect rect = [userInfo[UIKeyboardFrameBeginUserInfoKey] CGRectValue];
+    CGFloat keyboardHeight = CGRectGetHeight(rect);
+    CGFloat keyboardDuration = [userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    
+    // 修改下边距约束
+    _bottomConstraint.constant = keyboardHeight;
+    
+    // 更新约束
+    [UIView animateWithDuration:keyboardDuration animations:^{
+        
+        [self.view layoutIfNeeded];
+    }];
+}
+
+- (void)keyboardWillHideNotification:(NSNotification *)notification {
+    
+    // 获得键盘动画时长
+    NSDictionary *userInfo = [notification userInfo];
+    CGFloat keyboardDuration = [userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    
+    // 修改为以前的约束（距下边距20）
+    _bottomConstraint.constant = 20;
+    
+    // 更新约束
+    [UIView animateWithDuration:keyboardDuration animations:^{
+        
+        [self.view layoutIfNeeded];
+    }];
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+    [super touchesBegan:touches withEvent:event];
+    
+    [self.view endEditing:YES];
 }
 
 @end
