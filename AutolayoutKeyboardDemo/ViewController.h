057feca527ad851138ef0ec1e485ccb4f2fe72d5
TangJR <tangjr.work@gmail.com>
2015-04-29 09:21:11 +0800

    init

---
+++ AutolayoutKeyboardDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutolayoutKeyboardDemo
+//
+//  Created by TangJR on 15/4/29.
+//  Copyright (c) 2015年 tangjr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AutolayoutKeyboardDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AutolayoutKeyboardDemo
+//
+//  Created by TangJR on 15/4/29.
+//  Copyright (c) 2015年 tangjr. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
